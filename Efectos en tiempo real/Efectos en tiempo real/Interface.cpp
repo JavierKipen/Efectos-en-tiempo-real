6e114daa303e963cfe03237983be8c1063413f94
Javier Kipen <jkipen@itba.edu.ar>
2018-04-20 19:26:45 -0300

    Se añade la interfaz

--- Efectos en tiempo real/Efectos en tiempo real/Interface.cpp
+++ Efectos en tiempo real/Efectos en tiempo real/Interface.cpp
@@ -6,6 +6,22 @@ Interface::Interface()
 {
 }
 
+bool Interface::interact()
+{
+	printMenu();
+	return false;
+}
+
+
+void Interface::attachAudioEffects(AudioEffects * A)
+{
+	this->A = A;
+}
+
+
+void Interface::printMenu()
+{
+}
 
 Interface::~Interface()
 {
--- Efectos en tiempo real/Efectos en tiempo real/Interface.h
+++ Efectos en tiempo real/Efectos en tiempo real/Interface.h
@@ -4,6 +4,11 @@ class Interface
 {
 public:
 	Interface();
+	bool interact(); //Interacts with the user, changing the effects and exiting.
+	void attachAudioEffects(AudioEffects *A);
 	~Interface();
+private:
+	void printMenu();
+	AudioEffects * A;
 };
 
