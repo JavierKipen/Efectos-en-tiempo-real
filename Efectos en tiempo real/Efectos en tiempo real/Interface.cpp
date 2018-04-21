eb1ff9e340a6b5cce232fb13329dfc4366ce6bff
Javier Kipen <jkipen@itba.edu.ar>
2018-04-20 21:41:57 -0300

    Mas cambios, pal finde ta

--- Efectos en tiempo real/Efectos en tiempo real/Interface.cpp
+++ Efectos en tiempo real/Efectos en tiempo real/Interface.cpp
@@ -1,15 +1,45 @@
 #include "Interface.h"
 
+#define FIRST_MENU_MSG "Welcome to Audio Effects. This are the avaiable options"
+#define CHANGE_EFFECT "Select new effect"
+#define CHANGE_EFFECT_PARAMS "Change current effect settings"
+#define EXIT "Exit"
+#define CANCEL "Cancel"
+#define FIRST_MENU_OPTIONS {CHANGE_EFFECT, CHANGE_EFFECT_PARAMS, EXIT}
+
+
 
 
 Interface::Interface()
 {
+	currState = FIRST_MENU;
+	optionsToPrint = FIRST_MENU_OPTIONS;
+	printMenu();
 }
 
 bool Interface::interact()
 {
-	printMenu();
-	return false;
+	bool quit = false;
+	int ch = getch();	
+	if (isValid(ch))
+	{
+		switch (currState)
+		{
+		case FIRST_MENU:
+			quit = handleFirstMenu(ch - (int)'1'); //Solo se puede salir en el primer menú.
+			break;
+		case CHOOSE_EFFECT:
+			handleChooseEffect(ch - (int)'1');
+			break;
+		case CHOOSE_PARAM:
+			handleChooseParam(ch - (int)'1');
+			break;
+		case SET_PARAM_VALUE:
+			handleSetParamValue(ch - (int)'1');
+			break;
+		}
+	}
+	return quit;
 }
 
 
@@ -19,8 +49,64 @@ void Interface::attachAudioEffects(AudioEffects * A)
 }
 
 
+bool Interface::isValid(int ch)
+{
+	return (ch - (int)'1') <= optionsToPrint.size();
+}
+
+bool Interface::handleFirstMenu(unsigned int optChosen)
+{
+	bool retVal = false;
+	if (strcmp(optionsToPrint[optChosen].c_str(), CHANGE_EFFECT))
+	{
+		currState = FIRST_MENU;
+		optionsToPrint = LIST_OF_EFFECTS;
+		optionsToPrint.push_back(CANCEL);
+	}
+	else if (strcmp(optionsToPrint[optChosen].c_str(), CHANGE_EFFECT_PARAMS))
+	{
+		currState = FIRST_MENU;
+		optionsToPrint = A->getCurrParamNames();
+		optionsToPrint.push_back(CANCEL);
+	}
+	else if (strcmp(optionsToPrint[optChosen].c_str(), EXIT))
+		retVal = true;
+	if (!retVal)
+		printMenu();
+	return retVal;
+
+}
+
+void Interface::handleChooseEffect(unsigned int optChoosed)
+{
+	
+}
+
+void Interface::handleChooseParam(unsigned int optChosen)
+{
+	string aux;
+	if (strcmp(optionsToPrint[optChosen].c_str(), CANCEL))
+	{
+		currState = FIRST_MENU;
+		optionsToPrint = FIRST_MENU_OPTIONS;
+	}
+	else
+	{
+		currState = SET_PARAM_VALUE;
+		aux=optionsToPrint[optChosen];
+		optionsToPrint.clear();
+		optionsToPrint.push_back(aux);
+	}
+	printMenu();
+}
+
+void Interface::handleSetParamValue(unsigned int optChoosed)
+{
+}
+
 void Interface::printMenu()
 {
+	system("cls");
 }
 
 Interface::~Interface()
--- Efectos en tiempo real/Efectos en tiempo real/Interface.h
+++ Efectos en tiempo real/Efectos en tiempo real/Interface.h
@@ -1,14 +1,28 @@
 #pragma once
 #include "AudioEffects.h"
+#include <list>
+#include <conio.h>
+#include <Windows.h>
+
+typedef  enum { FIRST_MENU, CHOOSE_EFFECT, CHOOSE_PARAM, SET_PARAM_VALUE } INTERFACE_STATE;
 class Interface
 {
 public:
 	Interface();
 	bool interact(); //Interacts with the user, changing the effects and exiting.
 	void attachAudioEffects(AudioEffects *A);
+	
 	~Interface();
 private:
+	bool isValid(int ch);
+	bool handleFirstMenu(unsigned int optChoosed);
+	void handleChooseEffect(unsigned int optChoosed);
+	void handleChooseParam(unsigned int optChoosed);
+	void handleSetParamValue(unsigned int optChoosed);
 	void printMenu();
 	AudioEffects * A;
+	INTERFACE_STATE currState;
+	vector<string> optionsToPrint;
+	vector<string> valuesOfOptions;
 };
 
