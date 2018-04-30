a825f8127c816300c98c9ae8f35a05fd1715fde4
Javier Kipen <jkipen@itba.edu.ar>
2018-04-30 20:15:55 -0300

    Ponele que zafa ahi el multi Schroeder

--- Efectos en tiempo real/Efectos en tiempo real/Reverb.cpp
+++ Efectos en tiempo real/Efectos en tiempo real/Reverb.cpp
@@ -3,6 +3,9 @@
 #define REVERB_TYPE_INDEX 0
 #define REVERB_SCHR_G_INDEX 1
 #define REVERB_SCHR_T_DELAY_INDEX 2
+#define REVERB_SCHR_BOXES_INDEX 1
+#define REVERB_MULTI_SCHR_G_INDEX 2
+#define REVERB_MULTI_SCHR_T_DELAY_INDEX 3
 
 Reverb::Reverb(unsigned int sampleFreq)
 {
@@ -12,6 +15,11 @@ Reverb::Reverb(unsigned int sampleFreq)
 	maxSamplesNeeded = (unsigned int)(REVERB_SCHROEDER_MAX_T_DELAY  * (float)(sampleFreq)+1);
 	prevInputL.resize(maxSamplesNeeded, 0);
 	prevInputR.resize(maxSamplesNeeded, 0);
+	for (unsigned int i = 0; i < REVERB_SCHROEDER_MAX_BOXES; i++)
+	{
+		memoryL[i].resize(maxSamplesNeeded, 0);
+		memoryR[i].resize(maxSamplesNeeded, 0);
+	}
 	saveValues();
 	counter = 0;
 }
@@ -21,14 +29,19 @@ bool Reverb::Action(const float * in, float * out, unsigned int len)
 	float aux;
 	unsigned int delayedInputIndex = 0;
 	if (paramValues[REVERB_TYPE_INDEX] == "Schroeder")
+	{
 		for (unsigned int i = 0; i < len; i++) //Dos veces len por ser estereo.
 		{
-			*out = (-1) * schroederG * (*in) + prevInputL[counter];
-			prevInputL[counter] = (*in++) + schroederG * (*out++);
-			*out = (-1) * schroederG * (*in) + prevInputR[counter];
-			prevInputR[counter] = (*in++) + schroederG * (*out++);
+			*out++ = (-1) * schroederG * (*in) + prevInputL[counter] * (1 - schroederG * schroederG);
+			prevInputL[counter] = (*in++) + prevInputL[counter] * schroederG;
+			*out++ = (-1) * schroederG * (*in) + prevInputR[counter] * (1 - schroederG * schroederG);
+			prevInputR[counter] = (*in++) + prevInputR[counter] * schroederG;
 			counter = (++counter) % nmbrOfTaps;
 		}
+	}
+	else if (paramValues[REVERB_TYPE_INDEX] == "Multi_Schroeder")
+		computeMultipleSchroeder(in, out, len);
+
 	return true;
 }
 
@@ -39,7 +52,13 @@ bool Reverb::setParam(string paramName, string paramValue)
 	{
 		if (paramValue == "Schroeder")
 		{	paramValues[REVERB_TYPE_INDEX] = paramValue; retVal = true;}
-		else
+		else if (paramValue == "Multi_Schroeder")
+		{
+			paramNames = REVERB_SCHROEDER_MULTI_PARAM_NAMES;
+			paramValues = REVERB_SCHROEDER_MULTI_PARAM_VALUES;
+			saveValues();
+			retVal = true;
+		}
 			ErrorMsg = REVERB_TYPE_ERROR_MSG;
 	}
 	else if (paramName == "G Factor")
@@ -60,10 +79,55 @@ bool Reverb::setParam(string paramName, string paramValue)
 	return retVal;
 }
 
+void Reverb::computeMultipleSchroeder(const float * in, float * out, unsigned int len)
+{
+	float auxL[REVERB_SCHROEDER_MAX_BOXES+1];
+	float auxR[REVERB_SCHROEDER_MAX_BOXES+1];
+	for (unsigned int i = 0; i < len; i++) //Dos veces len por ser estereo.
+	{
+		auxL[0] = (*in++);
+		auxR[0] = (*in++);
+		for (unsigned int nmbrOfBox = 0; nmbrOfBox < nmbrOfBoxes; nmbrOfBox++)
+		{
+			auxL[nmbrOfBox+1] = (-1) * Gs[nmbrOfBox] * auxL[nmbrOfBox] + memoryL[nmbrOfBox][counters[nmbrOfBox]] * (1 - Gs[nmbrOfBox]* Gs[nmbrOfBox]);
+			memoryL[nmbrOfBox][counters[nmbrOfBox]] = auxL[nmbrOfBox] + memoryL[nmbrOfBox][counters[nmbrOfBox]] * Gs[nmbrOfBox];
+			auxR[nmbrOfBox + 1] = (-1) * Gs[nmbrOfBox] * auxR[nmbrOfBox] + memoryR[nmbrOfBox][counters[nmbrOfBox]] * (1 - Gs[nmbrOfBox] * Gs[nmbrOfBox]);
+			memoryR[nmbrOfBox][counters[nmbrOfBox]] = auxR[nmbrOfBox] + memoryR[nmbrOfBox][counters[nmbrOfBox]] * Gs[nmbrOfBox];
+		}
+		(*out++) = auxL[nmbrOfBoxes];
+		(*out++) = auxR[nmbrOfBoxes];
+		for(unsigned int j=0; j<nmbrOfBoxes; j++)
+			counters[j] = (++counters[j]) % nmbrsOfTaps[j];
+	}
+}
+
 void Reverb::saveValues()
 {
-	nmbrOfTaps = unsigned int(stof(paramValues[REVERB_SCHR_T_DELAY_INDEX]) * (float)(sampleFreq)+1);
-	schroederG = stof(paramValues[REVERB_SCHR_G_INDEX]);
+	if(paramValues[REVERB_TYPE_INDEX]== "Schroeder")
+	{
+		nmbrOfTaps = unsigned int(stof(paramValues[REVERB_SCHR_T_DELAY_INDEX]) * (float)(sampleFreq)+1);
+		schroederG = stof(paramValues[REVERB_SCHR_G_INDEX]);
+	}
+	else if (paramValues[REVERB_TYPE_INDEX] == "Multi_Schroeder")
+	{
+		nmbrOfBoxes = stof(paramValues[REVERB_SCHR_BOXES_INDEX]);
+		string aux1, aux2;
+		string::size_type szT,szG;
+		nmbrsOfTaps[0] = unsigned int(stof(paramValues[REVERB_MULTI_SCHR_T_DELAY_INDEX], &szT) * (float)(sampleFreq)+1);
+		Gs[0] = stof(paramValues[REVERB_MULTI_SCHR_G_INDEX], &szG);
+		counters[0] = 0;
+		aux1 = paramValues[REVERB_MULTI_SCHR_T_DELAY_INDEX];
+		aux2 = paramValues[REVERB_MULTI_SCHR_G_INDEX];
+		for (unsigned int i = 1; i < nmbrOfBoxes; i++)
+		{
+			aux1 = aux1.substr(szT);
+			aux2 = aux2.substr(szG);
+			nmbrsOfTaps[i] = unsigned int(stof(aux1, &szT) * (float)(sampleFreq)+1);
+			Gs[i] = stof(aux2, &szG);
+			counters[i] = 0;
+		}
+	}
+	
 }
 
 Reverb::~Reverb()
