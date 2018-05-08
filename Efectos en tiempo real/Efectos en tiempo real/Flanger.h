d8cec5039a5c7fd82a073d6664f23bf7250439bd
Marcos Brito <mbrito@itba.edu.ar>
2018-05-08 12:28:27 -0300

    Terminado el Flanger

--- Efectos en tiempo real/Efectos en tiempo real/Flanger.h
+++ Efectos en tiempo real/Efectos en tiempo real/Flanger.h
@@ -3,13 +3,14 @@
 #include "portaudio.h"
 #include <math.h>
 
-#define FLANGER_ATTENUATION_DEFAULT "0.8" 
-#define FLANGER_PEAK_DELAY_DEFAULT "10"	// not user controllable
-#define FLANGER_FREQ_DEFAULT "1" //Hz
-#define FLANGER_AVG_DELAY "2"	// not user controllable
-#define FLANGER_INVERT_DEFAULT "0"
+#define FLANGER_DELAY_DEFAULT "15"		// Base length of the delay line in ms.	[1:20]
+#define FLANGER_WIDTH_DEFAULT "10"	// Amplitude of the modulating signal in ms.	[1:20]
+#define FLANGER_DEPTH_DEFAULT "0.5"		// Mix amount of delayed signal [0:1]
+#define FLANGER_FEEDBACK_DEFAULT "0.4"	// Signal level of feedback signal
+#define FLANGER_LFO_DEFAULT "0.6"		//Hz [0.1:1]
+#define FLANGER_INVERT_DEFAULT "1"		// -1 or 1
 
-#define FLANGER_PARAMETERS {"ATTENUATION", "PEAK DELAY", "FREQUENCY", "INVERT"}
+#define FLANGER_PARAMETERS {"DELAY", "WIDTH", "DEPTH", "FEEDBACK", "LFO", "INVERT"}
 
 #define PI 3.14159265
 
@@ -21,10 +22,11 @@ public:
 	bool Action(const float * in, float * out, unsigned int len);
 	~Flanger();
 private:
-	vector<float> prevInput;
-	float attenuation, delayPeak, delayFreq;
-	unsigned int delaySize, invertSum;
-	float counter, delta;
+	vector<float> prevInputL, prevInputR;
+	int bufferSize;
+	float writeTapR, writeTapL;
+	float paramDelay, paramWidth, paramDepth, paramFB, paramLFO, paramInvert;
+	float lfoPhase;
 
 };
 
