2992682aa22a36534a4af43edc58a572abcccf7c
Javier Kipen <jkipen@itba.edu.ar>
2018-05-14 02:37:37 -0300

    Listo lo del wav

--- Efectos en tiempo real/Efectos en tiempo real/WavEffectAdder.cpp
+++ Efectos en tiempo real/Efectos en tiempo real/WavEffectAdder.cpp
@@ -2,20 +2,38 @@
 
 #define DEFAULT_SAMPLE_RATE 44100
 
+using namespace std::chrono;
+
 WavEffectAdder::WavEffectAdder(int argc, char *argv[])
 {
 	initOkay = false;
 	currEffect = NULL;
 	if (argc == 3) //En el caso que se haya escrito el programa, la localización del archivo de audio y el efecto a poner se sigue.
 	{
-		sfInfoR.format = 0;sfInfoR.samplerate = DEFAULT_SAMPLE_RATE;sfInfoR.channels = 2;
+		sfInfoR.format = 0; sfInfoR.samplerate = DEFAULT_SAMPLE_RATE; sfInfoR.channels = 2;
 		sfInfoW.format = 0; sfInfoW.samplerate = DEFAULT_SAMPLE_RATE; sfInfoW.channels = 2;
-		p2ReadFile=sf_open(argv[1], SFM_READ, &sfInfoR);
+		p2ReadFile = sf_open(argv[1], SFM_READ, &sfInfoR);
 		p2WriteFile = sf_open(("Output/" + (string)argv[1]).c_str(), SFM_WRITE, &sfInfoR);
-		if (p2ReadFile != NULL && p2WriteFile != NULL && chooseEffect((string)argv[2]))
+		if (p2ReadFile != NULL && p2WriteFile != NULL)
 		{
-			initOkay = true;
+			if (chooseEffect((string)argv[2]))
+			{
+				cout << START_OK_MSG;
+				_getch();
+				initOkay = true;
+			}
+			else
+				cout << EFFECT_CHOSEN_ERROR_MSG;
 		}
+		else
+			cout << WAV_FILENAME_ERROR_MSG;
+	}
+	else
+		cout << ARG_C_ERROR_MSG;
+	if (initOkay == false)
+	{
+		cout << ERROR_MSG;
+		_getch();
 	}
 }
 
@@ -26,12 +44,33 @@ bool WavEffectAdder::initOk()
 
 void WavEffectAdder::applyEffects()
 {
-	sf_count_t count, test;
+	sf_count_t count, end;
+	float progress = 0;
+	steady_clock::time_point  start_t, end_t;
+	duration<double> diff;
+	unsigned int loopsExecuted = 0;
+	end = sf_seek(p2ReadFile, 0, SEEK_END); //Para saber cuantos  bytes tiene el archivo a leer
+	sf_seek(p2ReadFile, 0, SEEK_SET);
+	start_t = high_resolution_clock::now();
 	do {
 		count = sf_read_float(p2ReadFile, bufferInput, MAX_INPUT_LEN);
 		currEffect->Action(bufferInput, bufferOutput, count/2);
-		test=sf_write_float(p2WriteFile, bufferOutput, count);
+		sf_write_float(p2WriteFile, bufferOutput, count);
+		loopsExecuted++;
+		end_t = high_resolution_clock::now();
+		diff = end_t - start_t;
+		if (diff.count() > 1 / FPS)
+		{
+			system("cls");
+			progress = (((float)MAX_INPUT_LEN * (float)loopsExecuted) / (float)(2*end))*100; 
+			printf("Progress: %.2f %%",progress);
+			start_t = high_resolution_clock::now();
+		}
+
 	} while (count == MAX_INPUT_LEN);
+	system("cls");
+	cout << FINISHED_OK_MSG;
+	_getch();
 }
 
 
--- Efectos en tiempo real/Efectos en tiempo real/WavEffectAdder.h
+++ Efectos en tiempo real/Efectos en tiempo real/WavEffectAdder.h
@@ -9,6 +9,22 @@
 #include "Vibrato.h"
 #include "Phaser.h"
 #include <iostream>
+#include <chrono>
+#include <thread>
+#include <Windows.h>
+#include <conio.h>
+
+#define START_OK_MSG "Welcome to real time effects!\n The .wav file that you've entered as second argument, \n will be passed through the effect you added in the third argument.\n Press any key to start processing\n"
+#define FINISHED_OK_MSG "The output file is in '.../Efectos en tiempo real/Output/' with the same name as the input.\n Press any key to exit.\n"
+
+#define ARG_C_ERROR_MSG "The arguments to call this program must be 1 or 3.\n 1 - Real time effects\n 3 - WAV Processing \n "
+#define WAV_FILENAME_ERROR_MSG "The second argument must be a .wav in the same folder as the solution.\n"
+#define EFFECT_CHOSEN_ERROR_MSG "The third argument must be one of the effets.\n This are the possible effects:\nDelay\nReverb\nDistortion\nPhaser\nVibrato\nChorus\nFlanger\n3D\n"
+
+#define ERROR_MSG "Press any key to end...\n"
+
+#define FPS 30.0f
+
 class WavEffectAdder
 {
 public:
